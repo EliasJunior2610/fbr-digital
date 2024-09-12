331097385f1ca30d48bb259a1368e87a41856220
EliasJunior2610 <e26junior2002@gmail.com>
2024-09-12 15:22:24 -0300

    alterações no Header

--- FBR Digital Projeto/src/components/Header/header.tsx
+++ FBR Digital Projeto/src/components/Header/header.tsx
@@ -4,7 +4,11 @@ import Logo from "../../assets/logo-fbr.png";
 function Header() {
     return(
         <div id="Header">
-           <a href="fbrdigital.com.br"><img src={Logo} alt="Logo FBR Digital" /></a> 
+            <a href="fbrdigital.com.br"><img src={Logo} alt="Logo FBR Digital" /></a>
+            <div id="Links">
+                <a href="../Registro/Registro.tsx"><p>Cadastro</p></a>
+                <a href="../Login/Login.tsx"><p>Login</p></a>
+            </div>
         </div>
     );
 }
