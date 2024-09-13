b5418187683841b9bda11385fe05d9b253b66b0a
EliasJunior2610 <e26junior2002@gmail.com>
2024-09-13 15:01:48 -0300

    Ajustando o rodapé

--- FBR Digital Projeto/src/components/Rodape/Rodape.tsx
+++ FBR Digital Projeto/src/components/Rodape/Rodape.tsx
@@ -1,40 +1,40 @@
 import Logo from "../../assets/Marca FBR.png";
 import "./Rodape.css";
 
-const Rodape: React.FC = () => {
+function Rodape(){
   return (
-    <div>
-      <footer className="container">
-        <div className="text">
-          <p>R. Maria de Lourdes Casé Porto, 51</p>
-          <p>Edif Times Business, salas 1701 a 1704</p>
-          <p>Bairro Maurício de Nassau Caruaru - PE</p>
-        </div>
-
-        <div className="container-button">
-          <div className="button-item1">
-            <button>
-              <a href="#">Solicitar Serviço</a>
-            </button>
+    <div id="baseboard">
+        <div id="textos">
+           <div id="text1">
+            <p>R. Maria de Lourdes Casé Porto, 51</p>
+            <p>Edif Times Business, salas 1701 a 1704</p>
+            <p>Bairro Maurício de Nassau Caruaru - PE</p>
           </div>
-
-          <div className="button-item2">
-            <button>
-              <a href="#">Download do Contrato Padrão</a>
-            </button>
+          
+          <div id="text2">
+            <p>comercial@fbrdigital.com.br</p>
+            <p>0800 081 4034 | (81) </p>
           </div>
         </div>
 
-        <div className="text">
-          <p>comercial@fbrdigital.com.br</p>
-          <p>0800 081 4034 | (81) </p>
+        <div id="container-button">
+          <div id="button-item1">
+            <button id="button1">
+              Solicitar Serviço
+            </button>
+          </div>
+
+          <div id="button-item2">
+            <button id="button2">
+              Download do Contrato Padrão
+            </button>
+          </div>
         </div>
+
 
         <figure>
           <img src={Logo} alt="Logo FBR" />
         </figure>
-
-      </footer>
     </div>
   );
 };
