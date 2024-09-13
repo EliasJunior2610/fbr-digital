77cfd7b620b1290f8004963ee02a8cd6dc2c6307
EliasJunior2610 <e26junior2002@gmail.com>
2024-09-13 10:59:33 -0300

    Adicionando rotas no App.tsx

--- FBR Digital Projeto/src/App.tsx
+++ FBR Digital Projeto/src/App.tsx
@@ -1,13 +1,29 @@
-import Header from "./components/Header/header"
-import Rodape from "./components/Rodape/Rodape"
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/header";
+import Rodape from "./components/Rodape/Rodape";
+import Registro from "./components/Registro/Registro";
+import Login from "./components/Login/Login";
 
+function App() {
   return (
-    <div>
-      <Header />
-      <Rodape />
-    </div>
-  )
+    <Router>
+      <Routes>
+        {/* Rotas renderizadas agora */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Rodape />
+            </>
+          }
+        />
+        {/* Links pra outras páginas */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- FBR Digital Projeto/src/components/Header/header.tsx
+++ FBR Digital Projeto/src/components/Header/header.tsx
@@ -1,25 +1,23 @@
 import "./Header.css";
 import Logo from "../../assets/Marca FBR.png";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Registro from "../Registro/Registro";
-import Login from "../Login/Login";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return(
-        <Router>
-            <div id="Header">
-                <a href="fbrdigital.com.br"><img src={Logo} alt="Logo FBR Digital" /></a>
-                <div id="Links">
-                    <Link to="/registro"><p>Cadastro</p></Link>
-                    <Link to="/login"><p>Login</p></Link>
-                </div>
-            </div>
-            <Routes>
-                <Route path="/registro" element={<Registro />} />
-                <Route path='/login' element={<Login />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div id="Header">
+      <a href="fbrdigital.com.br">
+        <img src={Logo} alt="Logo FBR Digital" />
+      </a>
+      <div id="Links">
+        <Link to="/registro">
+          <p>Cadastro</p>
+        </Link>
+        <Link to="/login">
+          <p>Login</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
